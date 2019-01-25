229027d45259a8f868e84620572365b131fe71a8
Zola <zolamcadie@gmail.com>
2019-01-25 14:30:22 -0500

    Add basic loading icon component with animation

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import CharacterPage from './pages/CharacterPage';
 import LoginPage from './pages/LoginPage';
 import Logout from './components/Logout';
 import Signup from './components/Signup';
+import LoadingIcon from './components/LoadingIcon';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: false,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch(`http://localhost:1268/api/user`, {
+      credentials: 'include'
+    }).then(res => {
+      return res.json()
+    }).then(
+      res => this.setState({ user: res, isLoading: false }),
+      err => { this.setState({ isLoading: false }); console.error(err) }
+    )
+  }
+
   render() {
     return (
       <Router>
-        <div>
+        <div id="router-container">
+          <div id="app-header">{`${this.state.user.username}`}</div>
+          <LoadingIcon />
           <Route exact path="/" render={() => <Redirect to="/login" />} />
           <Route path="/login" component={LoginPage} />
           <Route path="/logout" component={Logout} />
